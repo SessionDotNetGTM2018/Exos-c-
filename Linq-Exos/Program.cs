7733a2c5b8a7ec6f84649bf66532a5475366c4b0
YBAZAN <40167348+YBAZAN@users.noreply.github.com>
2018-08-06 16:17:47 +0200

    Exercice Linq sur les scores

---
+++ Linq-Exos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Exos
+{
+    class Program
+    {
+        private static int[] scores = new[] { 97, 92, 81, 60 };
+
+        static void Main(string[] args)
+        {
+            ScoresSuperieursA80();
+            ScoresParOrdreCroissant();
+            Console.ReadKey();
+        }
+
+        private static void ScoresSuperieursA80()
+        {
+            AfficherEntete("Scores > 80");
+
+            // Syntaxe de requête
+            var requete = from score in scores
+                          where score > 80
+                          select score;
+
+            // Syntaxe de méthode
+            var requete2 = scores.Where(x => x > 80);
+
+            AfficherResultats(requete);
+        }
+
+        private static void ScoresParOrdreCroissant()
+        {
+            AfficherEntete("Scores par ordre croissant");
+
+            // Syntaxe de requête
+            var requete = from score in scores
+                          orderby score
+                          select score;
+
+            // Syntaxe de méthode
+            var requete2 = scores.OrderBy(x => x);
+            AfficherResultats(requete2);
+        }
+
+        private static void AfficherResultats<T>(IEnumerable<T> resultats)
+        {
+            foreach (var resultat in resultats)
+                Console.WriteLine(resultat);
+        }
+
+        private static void AfficherEntete(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
